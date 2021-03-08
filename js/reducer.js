c235c5e552eeba7c25a75bae9b19e108ffe59e23
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-03-08 18:24:24 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,80 @@
+let state = {count: 0};
+
 function changeState(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
-    default:
-      return state;
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
   }
+
+function render(){
+    document.body.textContent = state.count
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+function dispatch(action){
+  state = changeState(state, action)
+  render()
+}
 
-changeState(state, action)
+render() 
+
+
+// SET 2
+// let state = {count: 0};
+
+// function changeState(state, action){
+//     switch (action.type) {
+//       case 'INCREASE_COUNT':
+//         return {count: state.count + 1}
+//       default:
+//         return state;
+//     }
+//   }
+
+// function dispatch(action){
+//   state = changeState(state, action)
+//   return state
+// }
+
+// function render(){
+//   document.body.textContent = state.count
+// }
+
+// function dispatch(action){
+//   state = changeState(state, action)
+//   render()
+// }
+
+// dispatch({type: 'INCREASE_COUNT'})
+// dispatch({type: 'INCREASE_COUNT'}) 
+
+
+// // call the render function
+// render() 
+
+
+// dispatch({type: 'INCREASE_COUNT'})
+//   // => {count: 1}
+// dispatch({type: 'INCREASE_COUNT'})
+//   // => {count: 2}
+// dispatch({type: 'INCREASE_COUNT'})
+//   // => {count: 3} 
+
+
+
+// Set 1
+  // function changeState(state, action){
+//   switch (action.type) {
+//     case 'INCREASE_COUNT':
+//       return {count: state.count + 1}
+//     default:
+//       return state;
+//   }
+// }
+
+// let state = {count: 0}
+// let action = {type: 'INCREASE_COUNT'}
+
+// changeState(state, action)
